d8b311c3c60a48c783ad62b1f2e9a6b6f8323fa4
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-01-04 13:10:45 +0100

    Texture parsing more flexible

--- srcs/parser/add_texture_to_struct.c
+++ srcs/parser/add_texture_to_struct.c
@@ -6,7 +6,7 @@
 /*   By: tamighi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/01 12:45:51 by tamighi           #+#    #+#             */
-/*   Updated: 2022/01/02 09:13:58 by tamighi          ###   ########.fr       */
+/*   Updated: 2022/01/04 13:09:19 by tamighi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,25 @@
 
 char	*go_to_path(char *line)
 {
-	int	i;
+	int		i;
+	int		j;
+	char	*path;
 
 	i = 0;
-	while (line[i] != ' ')
+	j = 0;
+	while (line[i] != ' ' || line[i] == '	')
 		i++;
-	i++;
-	return (&line[i]);
+	while (line[i] == ' ' || line[i] == '	')
+		i++;
+	while (line[i + j] && line[i + j] != ' ' && line[i + j] != '	')
+		j++;
+	path = malloc(j + 1);
+	j = 0;
+	while (line[i] && line[i] != ' ' && line[i] != '	')
+		path[j++] = line[i++];
+	path[j] = '\0';
+	printf("%s|\n", path);
+	return (path);
 }
 
 int	add_img(char *line, t_cub *cub, int nb)
@@ -32,6 +44,7 @@ int	add_img(char *line, t_cub *cub, int nb)
 		return (-1);
 	line = go_to_path(line);
 	img.img = mlx_xpm_file_to_image(cub->mlx.mlx, line, &img.w, &img.h);
+	free(line);
 	if (!img.img)
 		return (-1);
 	cub->textures[nb - 1] = img;
@@ -56,8 +69,9 @@ int	add_texture_to_struct(char *line, t_cub *cub)
 	i = 0;
 	while (line[i])
 	{
-		if (line[i] != ' ')
+		if (line[i] != ' ' && line[i] != '	')
 			return (-1);
+		i++;
 	}
 	return (1);
 }
