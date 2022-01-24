516aeb6b0c4fd93cdb8ebed5a4a07f0171b48409
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-01-24 09:10:44 +0100

    bug fix

--- srcs/global/wr_and_ex.c
+++ srcs/global/wr_and_ex.c
@@ -6,7 +6,7 @@
 /*   By: tamighi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/01 10:26:53 by tamighi           #+#    #+#             */
-/*   Updated: 2022/01/20 10:25:55 by tamighi          ###   ########.fr       */
+/*   Updated: 2022/01/24 09:02:01 by tamighi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,5 @@ void	wr_and_ex(char *msg, int ex)
 {
 	if(msg)
 		my_write(msg);
-	my_write("Exit.\n");
 	exit(ex);
 }
