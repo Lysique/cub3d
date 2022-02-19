a56adfa8952f5aa31dea1ab18b75dff114965964
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-02-19 16:38:29 +0100

    ennemy_moving

--- bonus/srcs/loop_hook/ennemy_manager.c
+++ bonus/srcs/loop_hook/ennemy_manager.c
@@ -6,14 +6,40 @@
 /*   By: tamighi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 14:46:21 by tamighi           #+#    #+#             */
-/*   Updated: 2022/02/19 14:50:32 by tamighi          ###   ########.fr       */
+/*   Updated: 2022/02/19 16:36:02 by tamighi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/loop_hook.h"
 
+void	ennemy_path(t_en *en, t_cub *cub)
+{
+	float	delta_x;
+	float	delta_y;
+	float	slope;
+	float	add;
+
+	delta_x = cub->player.x - en->x;
+	delta_y = cub->player.y - en->y;
+	slope = delta_y * -1 / delta_x;
+	en->angle = atan(slope) * PI;
+	add = fabsf(delta_x) + fabsf(delta_y);
+	if (add < 1)
+		return ;
+	if (abs((int)delta_x) > abs((int)delta_y))
+		en->x += 0.05 * (delta_x / add);
+	else if (abs((int)delta_x) < abs((int)delta_y))
+		en->y += 0.05 * (delta_y / add);
+	else
+	{
+		en->x += 0.05 * (delta_x / add);
+		en->y += 0.05 * (delta_y / add);
+	}
+}
+
 void	manage_ennemy(t_en *en, t_cub *cub)
 {
+	ennemy_path(en, cub);
 	(void)cub;
 	(void)en;
 }
