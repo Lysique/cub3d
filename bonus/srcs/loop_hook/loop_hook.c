3d34ea2bfeb3d6b779692742d77ed532fa57a5f0
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-02-21 13:03:07 +0100

    mlx fix

--- bonus/srcs/loop_hook/loop_hook.c
+++ bonus/srcs/loop_hook/loop_hook.c
@@ -6,7 +6,7 @@
 /*   By: tuytters <tuytters@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 13:05:35 by tamighi           #+#    #+#             */
-/*   Updated: 2022/02/19 14:46:43 by tamighi          ###   ########.fr       */
+/*   Updated: 2022/02/21 13:01:00 by tamighi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,15 @@
 
 int	loop_hook(t_cub *cub)
 {
+	static int	i = 0;
 	static t_time	time = 0;
 
 	cub->time = get_time() - time;
+
+	if (++i / 100 == 0)
+		return (0);
+	else
+		i = 0;
 	time = get_time();
 	door_manager(cub);
 	player_manager(cub);
