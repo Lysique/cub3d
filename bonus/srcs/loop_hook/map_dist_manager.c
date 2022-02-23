394391ff90e2f9869758492f0e66648a648653f0
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-02-23 10:22:34 +0100

    added file

---
+++ bonus/srcs/loop_hook/map_dist_manager.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_dist_manager.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tamighi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 14:30:56 by tamighi           #+#    #+#             */
+/*   Updated: 2022/02/23 09:45:34 by tamighi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/loop_hook.h"
+
+void	water_fill(t_cub *cub, int y, int x)
+{
+	if (cub->map_dist[y][x + 1] != -1
+		&& cub->map_dist[y][x + 1] > cub->map_dist[y][x] + 1)
+	{
+		cub->map_dist[y][x + 1] = cub->map_dist[y][x] + 1;
+		water_fill(cub, y, x + 1);
+	}
+	if (cub->map_dist[y][x - 1] != -1
+		&& cub->map_dist[y][x - 1] > cub->map_dist[y][x] + 1)
+	{
+		cub->map_dist[y][x - 1] = cub->map_dist[y][x] + 1;
+		water_fill(cub, y, x - 1);
+	}
+	if (cub->map_dist[y + 1][x] != -1
+		&& cub->map_dist[y + 1][x] > cub->map_dist[y][x] + 1)
+	{
+		cub->map_dist[y + 1][x] = cub->map_dist[y][x] + 1;
+		water_fill(cub, y + 1, x);
+	}
+	if (cub->map_dist[y - 1][x] != -1
+		&& cub->map_dist[y - 1][x] > cub->map_dist[y][x] + 1)
+	{
+		cub->map_dist[y - 1][x] = cub->map_dist[y][x] + 1;
+		water_fill(cub, y - 1, x);
+	}
+}
+
+void	reinitialize_map_dist(t_cub *cub)
+{
+	int	i;
+	int	j;
+	int	door;
+
+	j = -1;
+	while (cub->map[++j])
+	{
+		i = -1;
+		while (cub->map[j][++i])
+		{
+			if (cub->map_dist[j][i] != -1)
+				cub->map_dist[j][i] = INT_MAX;
+			door = get_door_index(cub->doors, j, i);
+			if (door != -1)
+			{
+				if (cub->doors[door].open == 1)
+					cub->map_dist[j][i] = INT_MAX;
+				else
+					cub->map_dist[j][i] = -1;
+			}
+		}
+	}
+}
+
+void	map_dist_manager(t_cub *cub)
+{
+	reinitialize_map_dist(cub);
+	cub->map_dist[(int)cub->player.y][(int)cub->player.x] = 0;
+	water_fill(cub, (int)cub->player.y, (int)cub->player.x);
+	
+	/*
+	int	i = -1;
+	int	j = -1;
+	while (cub->map[++j])
+	{
+		while (cub->map[j][++i])
+			printf("%d", cub->map_dist[j][i]);
+		printf("\n");
+		i = -1;
+	}
+	*/
+}
