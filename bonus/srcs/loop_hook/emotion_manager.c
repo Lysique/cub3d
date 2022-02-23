85a5d0e1e419ba18a4b426209bdd7c0661a34216
Thibault Uyttersprot <tuytters@fu-r7-p5.s19.be>
2022-02-23 15:00:37 +0100

    sang

--- bonus/srcs/loop_hook/emotion_manager.c
+++ bonus/srcs/loop_hook/emotion_manager.c
@@ -6,7 +6,7 @@
 /*   By: tuytters <tuytters@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 11:02:11 by tamighi           #+#    #+#             */
-/*   Updated: 2022/02/23 10:34:48 by tuytters         ###   ########.fr       */
+/*   Updated: 2022/02/23 14:59:36 by tuytters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,12 @@
 
 void	emotion_manager(t_cub *cub)
 {
-	static t_time	emo = 0;
-
-	if (cub->player.emotion != 1 || cub->player.emotion != 3
-		|| cub->player.emotion != 4)
-		emo += cub->time;
-	if (emo >= 750)
-	{
-		if (cub->player.life < 4)
+		if (cub->player.life < 4 && cub->player.life > 1)
 			cub->player.emotion = 4;
-		if (cub->player.life < 2)
+		else if (cub->player.life < 2)
 			cub->player.emotion = 3;
 		else
 			cub->player.emotion = 1;
-		emo = 0;
-	}
 	if (cub->action == SHOOTING)
 	{
 		if (cub->gun_type == 0)
