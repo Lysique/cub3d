39685c10b3369d97a787703c437d6251ff57c830
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-02-26 15:04:09 +0100

    fix parsing

--- bonus/includes/parser.h
+++ bonus/includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: tuytters <tuytters@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/01 10:03:00 by tamighi           #+#    #+#             */
-/*   Updated: 2022/02/22 10:17:13 by tamighi          ###   ########.fr       */
+/*   Updated: 2022/02/26 14:41:05 by tamighi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ void		map_checker(char **map);
 void		player_init(t_cub *cub);
 void		doors_init(t_cub *cub);
 void		ennemy_init(t_cub *cub);
+void		add_enemy(t_cub *cub, char c, int j, int i);
 void		map_dist_init(t_cub *cub);
 
 int			is_texture_line(char *line);
