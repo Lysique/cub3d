50084139315fca0746b3ec92fd0b39597e70df8c
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-02-27 10:18:05 +0100

    Swap_map implementation

---
+++ bonus/srcs/global/variables_init.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   variables_reset.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tamighi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 08:39:34 by tamighi           #+#    #+#             */
+/*   Updated: 2022/02/27 09:13:37 by tamighi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/global.h"
+
+void	gun_init(t_cub *cub)
+{
+	cub->gun[0].sprite = 0;
+	cub->gun[0].bullets = SG_BULLETS;
+	cub->gun[0].max_bullets = SG_BULLETS;
+	cub->gun[0].nb_sprites = 12;
+	cub->gun[0].nb_spr_shoot = 1;
+	cub->gun[0].time = 0;
+	cub->gun[0].status = GS_FREE;
+	cub->gun[0].damage = 10;
+	cub->gun[1].sprite = 0;
+	cub->gun[1].bullets = MG_BULLETS;
+	cub->gun[1].max_bullets = MG_BULLETS;
+	cub->gun[1].nb_sprites = 13;
+	cub->gun[1].nb_spr_shoot = 2;
+	cub->gun[1].status = GS_FREE;
+	cub->gun[1].damage = 2;
+}
+
+void	textures_init(t_cub *cub)
+{
+	int	i;
+
+	i = 0;
+	while (i < 7)
+		cub->textures[i++].img = 0;
+}
+
+void	key_init(t_cub *cub)
+{
+	cub->key.w = 0;
+	cub->key.s = 0;
+	cub->key.a = 0;
+	cub->key.d = 0;
+	cub->key.right = 0;
+	cub->key.left = 0;
+	cub->key.door_r = 1;
+	cub->key.door_p = 0;
+	cub->key.reload = 0;
+	cub->key.shoot_r = 1;
+	cub->key.shoot_p = 0;
+	cub->key.swap_gun = 0;
+	cub->mouse.x = WIN_W / 2;	
+}
+
+void	variables_init(t_cub *cub)
+{
+	cub->time = 0;
+	cub->gun_type = 0;
+	cub->map = 0;
+	cub->map_dist = 0;
+	cub->doors = 0;
+	cub->player.life = 5;
+	cub->player.emotion = 1;
+	cub->en = 0;
+	cub->action = FREE_GUN;
+	cub->game_state = GAME;
+	key_init(cub);
+	textures_init(cub);
+	gun_init(cub);
+}
